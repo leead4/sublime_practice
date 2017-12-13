56401d76bf0979ac0eb39368508c921a20c7233b
leead4 <leead4@gmail.com>
2017-12-13 16:39:44 -0600

    good stuff here

---
+++ new.js
@@ -0,0 +1,23 @@
+// var = a;
+
+
+var list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+var output = [];
+
+for (var i = 0; i < list.length; i++){
+	var addMe = list.slice(0, 3);
+		output.push(addMe);
+		console.log(output);
+	// console.log();	
+	// console.log();	
+}
+
+console.log("here", output);
+console.log(output);
+	// console.log();	
+	// console.log();	
+}
+
+console.log("here", output);
+
+// output = ['abc', 'def', 'ghi', ...];
